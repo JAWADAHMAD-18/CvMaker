526c02b1cb1f37f0063d07c35b6734ff630a4e8c
Jawad Ahmad <jawadahmad5618@gmail.com>
2025-07-23 20:24:06 +0500

    a heading

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="min-h-screen bg-gray-100 px-4 py-6">
+      <h1 className="text-2xl font-bold text-center text-gray-800">Cv Maker</h1>
       <ProgressDots />
       <div className="max-w-3xl mx-auto">
         <Outlet />
